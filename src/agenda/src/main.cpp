bf5be885012bef04cf9a40ec9687a2ee86247cdf
Ruben Smit <RubenSmit@users.noreply.github.com>
2021-05-19 00:23:54 +0200

    Open redirect link and get calendar items

--- src/agenda/src/main.cpp
+++ src/agenda/src/main.cpp
@@ -1,11 +1,53 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "SSID";
-const char* password = "PASSWORD";
+const char* ssid     = "ssid";
+const char* password = "password";
 
 const char* host = "script.google.com";
-const char* url = "replace_with_url";
+const char* redirectHost = "script.googleusercontent.com";
+const char* url = "/macros/s/your-random-code/exec";
+
+String httpsGet(String host, String url) {
+  // Use WiFiClient class to create TCP connections
+  WiFiClientSecure client;
+  client.setInsecure(); // this is the magical line that makes everything work
+
+  const int httpPort = 443; // 80 is for HTTP / 443 is for HTTPS
+  if (!client.connect(host, httpPort)) { //works!
+    Serial.println("connection failed");
+    return "";
+  }
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  String response = "";
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+    response.concat(c);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+
+  return response;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -33,40 +75,21 @@ void loop() {
 
   Serial.print("connecting to ");
   Serial.println(host);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClientSecure client;
-  const int httpPort = 443; // 80 is for HTTP / 443 is for HTTPS!
   
-  client.setInsecure(); // this is the magical line that makes everything work
-  
-  if (!client.connect(host, httpPort)) { //works!
-    Serial.println("connection failed");
-    return;
-  }
+  String redirectMessage = httpsGet(host, url);
 
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
+  Serial.println("Redirect message: ");
+  Serial.println(redirectMessage);
 
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
+  int from = redirectMessage.indexOf("/macros");
+  int to = redirectMessage.indexOf("\">here");
 
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      Serial.println("headers received");
-      break;
-    }
-  }
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
+  String redirectUrl = redirectMessage.substring(from, to);
+  redirectUrl.replace("&amp;", "&");
+  Serial.print("Redirect url: ");
+  Serial.println(redirectUrl);
 
-  Serial.println();
-  Serial.println("closing connection");
+  String calenderItems = httpsGet(redirectHost, redirectUrl);
+
+  delay(50000);
 }
